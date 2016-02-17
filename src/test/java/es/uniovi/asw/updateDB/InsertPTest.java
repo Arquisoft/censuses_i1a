ffd57a51f9c916a43a06ebe93ef16a000751bd94
SantiagoBarro <sbipo93@gmail.com>
2016-02-18 00:13:02 +0100

    Database uploaded, connection and insert voters classes. Tests to do.

---
+++ src/test/java/es/uniovi/asw/updateDB/InsertPTest.java
@@ -0,0 +1,14 @@
+package es.uniovi.asw.updateDB;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InsertPTest {
+
+	@Test
+	public void test() {
+		fail(""); //TODO
+	}
+
+}
